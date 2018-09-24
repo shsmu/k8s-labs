277f42c81a984a8b546fd0909abb73571aece51f
root <root@localhost>
2018-09-24 12:42:44 +0800

    vm for jenkins

---
+++ Hyper-V/PowerShell/create_vm_jenkins.ps1
@@ -0,0 +1,25 @@
+# 以管理员身份运行
+set-executionpolicy remotesigned
+
+
+$VMName = "CI"
+$VMMAC = "00-00-00-00-02-31"
+$MemoryStartupBytes = 2147483648
+$NewVHDSizeBytes = 303687091200
+$SwitchName = "办公"
+$VM = @{
+    Name = $VMName
+    MemoryStartupBytes = $MemoryStartupBytes
+    Generation = 1
+    NewVHDPath = " D:\Hyper-V\$VMName\Virtual Hard Disks\$VMName.vhdx"
+    NewVHDSizeBytes = $NewVHDSizeBytes
+#  BootDevice = "VHD"
+    BootDevice = "LegacyNetworkAdapter"
+    Path = " D:\Hyper-V\$VMName "
+#  SwitchName = (get-vmswitch).Name[0]
+    SwitchName = $SwitchName
+}
+
+New-VM @VM
+Set-VM -Name $VMName -ProcessorCount 2
+Set-VMNetworkAdapter -VMName $VMName -StaticMacAddress $VMMAC
